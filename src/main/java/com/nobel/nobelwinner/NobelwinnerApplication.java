2897f264c6a3e9a8908571766caba4651e96751d
Vicky kaushal <121958580+Vicky-koushal-2@users.noreply.github.com>
2023-12-30 19:06:41 +0530

    first commit

---
+++ src/main/java/com/nobel/nobelwinner/NobelwinnerApplication.java
@@ -0,0 +1,13 @@
+package com.nobel.nobelwinner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NobelwinnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NobelwinnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nobel/nobelwinner/NobelwinnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nobel.nobelwinner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NobelwinnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
